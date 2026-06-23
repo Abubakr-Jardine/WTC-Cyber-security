52f71ff1fb4248c2474782cb3c8c332d2acf1292
Abubakr Jardine <abjarcpt025@student.wethinkcode.co.za>
2026-06-23 18:52:25 +0200

    Feature: Button to login page

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, request,flash,url_for
+from flask import Flask, request,url_for, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def welcome_page():
-    return "Welcome"
+@app.route("/",methods = ["POST","GET"])
+def index():
+    if request.method == "POST":
+        if request.form.get("action1") == "Login":
+            return redirect(url_for("login_page"))
+    return render_template("index.html")
+
+@app.route("/login")
+def login_page():
+    return "This is the login page"
